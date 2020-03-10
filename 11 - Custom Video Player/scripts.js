90615f144729ebfb4ec429b3ecc1945125518bba
Edgar Lopez <edgarosv.lz@gmail.com>
2020-03-10 14:04:19 -0600

    Exercise 11 complete

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -27,18 +27,52 @@ function updateButton(){
 }
 
 function skip(){
-    console.log(this.dataset.skip);
+    //console.log(this.dataset.skip);
+    video.currentTime += parseFloat(this.dataset.skip);
+}
 
+function handleRangeUpdate(){
+    video[this.name] = this.value;
+}
+
+function handleProgress(){
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e){
+    const scrubTime = video.duration*(e.offsetX / progress.offsetWidth);
+    video.currentTime = scrubTime;
 }
 
 /* Hook up the event listeners */
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
-skipButtons.forEach(button => button.addEventListener('click',skip));
+video.addEventListener('timeupdate', handleProgress);
+
 toggle.addEventListener('click',togglePlay);
+
 window.addEventListener('keydown', e => {
     if (e.keyCode === 32){
         togglePlay();
     }
+    if (e.keyCode === 39) {
+        video.currentTime+=25;
+    }
+    if (e.keyCode === 37){
+        video.currentTime-=10;
+    }
 });
+
+skipButtons.forEach(button => button.addEventListener('click',skip));
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown',() => mousedown =  true);
+progress.addEventListener('mouseup',() => mousedown =  false);
+progress.addEventListener('mouseout',() => mousedown =  false);
+
